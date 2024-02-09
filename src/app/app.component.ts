dd27633df48c2cf245664a905c1703b7f038358f
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-09 19:35:03 -0300

    feat: Footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from "./layout/top-bar/top-bar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, TopBarComponent]
+    imports: [RouterOutlet, TopBarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'lojitas-angular';
