844fd5674f49786e4133dd6d7d176c9226b731a7
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-09 20:23:28 -0300

    feat: Product card catalog component

--- src/app/pages/home/components/products/products.component.ts
+++ src/app/pages/home/components/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PRODUCTS } from '../../../../data/product.data';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
